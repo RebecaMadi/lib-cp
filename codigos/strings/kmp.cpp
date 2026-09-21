ca13746e0157af0a3188a64dae4963e13cc9357f
Rebeca Madi <64318502+RebecaMadi@users.noreply.github.com>
2026-09-21 12:56:16 -0300

    Update kmp.cpp

--- codigos/strings/kmp.cpp
+++ codigos/strings/kmp.cpp
@@ -1,38 +1,50 @@
-// funcao de prefixo
-vector<int> PI(string s)
-{
-    vector<int> p(s.size()+1, 0);
-
-    for(int i = 1, j = 0; i < s.size(); i++)
-    {
-        while(j > 0 && s[i] != s[j]) j = p[j-1];
-        if(s[i] == s[j]) j++;
+vector<int> PI(string s) {
+    int n = s.size();
+    vector<int> p(n);
+    for (int i = 1, j = 0; i < n; i++) {
+        while (j > 0 && s[i] != s[j]) j = p[j - 1];
+        if (s[i] == s[j]) j++;
         p[i] = j;
     }
+
     return p;
 }
 
-// KMP autômato
 struct autKMP {
     vector<vector<int>> nxt;
+    int m;
 
-    autKMP(string s) : nxt(26, vector<int>(s.size()+1)) {
-        vector<int> p = PI(s+'$');
-        nxt[s[0]-'A'][0] = 1;
-        for(char c = 0; c<26; c++) // nova transição
-            for(int j = 1; j <= s.size(); j++) // tamanho
-                nxt[c][j] = c == s[j] ? j+1 : nxt[c][p[j-1]], freq[nxt[c][j]]++;
-     }
+    autKMP(string s) {
+        m = s.size();
+        vector<int> p = PI(s);
+
+        // estados 0 ... m
+        nxt.assign(26, vector<int>(m + 1));
+
+        for (int c = 0; c < 26; c++) {
+            // estado 0
+            nxt[c][0] = (c == s[0] - 'a');
+            for (int j = 1; j < m; j++) {
+                if (c == s[j] - 'a') nxt[c][j] = j + 1;
+                else nxt[c][j] = nxt[c][p[j - 1]];
+            }
+            // estado m: acabamos de encontrar o padrão
+            nxt[c][m] = nxt[c][p[m - 1]];
+        }
+    }
 };
 
 int matching(string t, string s) {
-    int match = 0;
+    if (s.empty()) return 0;
     auto aut = autKMP(s);
     int at = 0;
-    for(int i=0; i<sz(t); i++)
-    {
-        at = aut.nxt[t[i]-'A'][at];
-        if(at == sz(s)) match++;
+    int match = 0;
+    for (char ch : t) {
+        at = aut.nxt[ch - 'a'][at];
+        if (at == s.size()) {
+            match++;
+            at = aut.nxt[ch - 'a'][at];
+        }
     }
     return match;
 }
