9af002ddc50608faf99afc032c672c4eb944e45c
Rebeca Madi <64318502+RebecaMadi@users.noreply.github.com>
2026-09-21 13:03:02 -0300

    Update kmp.cpp

--- codigos/strings/kmp.cpp
+++ codigos/strings/kmp.cpp
@@ -6,45 +6,36 @@ vector<int> PI(string s) {
         if (s[i] == s[j]) j++;
         p[i] = j;
     }
-
     return p;
 }
-
+ 
 struct autKMP {
     vector<vector<int>> nxt;
+    vector<int> pi;
     int m;
-
+ 
     autKMP(string s) {
         m = s.size();
-        vector<int> p = PI(s);
-
-        // estados 0 ... m
+        pi = PI(s);
         nxt.assign(26, vector<int>(m + 1));
-
+ 
         for (int c = 0; c < 26; c++) {
-            // estado 0
             nxt[c][0] = (c == s[0] - 'a');
-            for (int j = 1; j < m; j++) {
-                if (c == s[j] - 'a') nxt[c][j] = j + 1;
-                else nxt[c][j] = nxt[c][p[j - 1]];
+            for (int j = 1; j <= m; j++) {
+                if (j < m && c == s[j] - 'a') nxt[c][j] = j + 1;
+                else nxt[c][j] = nxt[c][pi[j - 1]];
             }
-            // estado m: acabamos de encontrar o padrão
-            nxt[c][m] = nxt[c][p[m - 1]];
         }
     }
 };
-
+ 
 int matching(string t, string s) {
-    if (s.empty()) return 0;
     auto aut = autKMP(s);
     int at = 0;
     int match = 0;
-    for (char ch : t) {
-        at = aut.nxt[ch - 'a'][at];
-        if (at == s.size()) {
-            match++;
-            at = aut.nxt[ch - 'a'][at];
-        }
+    for (char c : t) {
+        at = aut.nxt[c - 'a'][at];
+        if(at == s.size()) match++;
     }
     return match;
 }
