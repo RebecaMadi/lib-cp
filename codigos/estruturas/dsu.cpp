33968c19199821935a125106da31a19740dc747a
Rebeca Madi <64318502+RebecaMadi@users.noreply.github.com>
2026-09-23 13:27:12 -0300

    Update dsu.cpp

--- codigos/estruturas/dsu.cpp
+++ codigos/estruturas/dsu.cpp
@@ -1,43 +1,39 @@
 struct DSU {
-    vector<int> parent, rank, size;
-    int n;
-
-    DSU(int n)
-    {
-        this->n = n+1;
-        parent.assign(this->n, 0);
-        rank.assign(this->n, 0);
-        size.assign(this->n, 0);
+    vector<int> parent, size;
+    int n, gt;
+ 
+    DSU(int n) {
+        this->n = n + 1;
+        parent.resize(this->n);
+        size.assign(this->n, 1);
+        gt = 1;
+        for(int i = 1; i <= n; i++) make_set(i);
     }
-
-    void make_set(int v)
-    {
-        parent[v] = v;crank[v] = 0; size[v] = 1;
+ 
+    void make_set(int v) {
+        parent[v] = v;
+        size[v] = 1;
     }
-
-    int find_set(int v)
-    {]
+ 
+    int find_set(int v) {
         return v == parent[v] ? v : parent[v] = find_set(parent[v]);
     }
-
-    void union_sets(int a, int b)
-    {
+ 
+    void union_sets(int a, int b) {
         a = find_set(a);
         b = find_set(b);
-
-        if(a!=b){
-            if(rank[a] < rank[b]) swap(a, b);
-            parent[b] = a;
-
-            // altura = max(rank[a], rank[b]), se rank[a] > rank[b] nada muda, se forem iguais então a altura aumenta
-            if(rank[a] == rank[b]) rank[a]++; 
-            size[a] += size[b];
-        }
+ 
+        if(a == b) return;
+ 
+        // small-to-large
+        if(size[a] < size[b]) swap(a, b);
+        parent[b] = a;
+        size[a] += size[b];
+        gt = max(gt, size[a]);
     }
-
-    int query(int v)
-    {
+ 
+    int query(int v) {
         v = find_set(v);
-        return (int) (size[v] * (size[v]-1))/2; // alterar dependendo do que se queira consultar, nesse caso é o número de pares dentro do mesmo conjunto
+        return 1LL * size[v] * (size[v] - 1) / 2;
     }
 };
